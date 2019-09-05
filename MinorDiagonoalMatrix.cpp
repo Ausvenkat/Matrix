12d671abbf8ddf80b56bb17ae168c929f87b4573
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-09-05 12:35:42 +0530

    Create MinorDiagonoalMatrix.cpp

---
+++ MinorDiagonoalMatrix.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int m,n,first[10][10],sum=0;
+  cin>>m>>n;
+  bool flag=true;
+  for(int i=0;i<m;i++){
+      for(int j=0;j<n;j++){
+          cin>>first[i][j];
+        }
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i+j==2){
+             sum+=first[i][j];
+            }
+        }
+    }
+    cout<<sum;
+  
+    return 0;  
+} 
